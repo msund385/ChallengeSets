fa802642eb2adca6467ea34fa84f6b4d918072cb
msund385 <ms43004foru@gmail.com>
2020-09-11 19:39:35 -0400

    ChallengeSets1 Done!

--- ChallengeSets/ChallengeSet01.cs
+++ ChallengeSets/ChallengeSet01.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+
+
+
 namespace ChallengeSets
 {
     public class ChallengeSet01
@@ -6,26 +9,34 @@ namespace ChallengeSets
         public bool AreTwoNumbersTheSame(int num1, int num2)
         {
             return num1 == num2;
+            
         }
 
         public double Subtract(double minuend, double subtrahend)
         {
-            throw new NotImplementedException();
+             
+             return minuend - subtrahend;
+            
+
+            
         }
 
         public int Add(int number1, int number2)
         {
-            throw new NotImplementedException();
+            return number1 + number2;
+            
         }
 
         public int GetSmallestNumber(int number1, int number2)
         {
-            throw new NotImplementedException();
+            return  Math.Min(number1, number2);
+            
         }
 
         public long Multiply(long factor1, long factor2)
         {
-            throw new NotImplementedException();
+            return factor1 * factor2;
+            
         }
 
         public string GetGreeting(string nameOfPerson)
